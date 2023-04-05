b72923d09a3b4a400ee95465052aeced8490544c
Venkatesh <112744029+192110481@users.noreply.github.com>
2023-04-05 22:06:50 +0530

    Add files via upload

---
+++ assignment2 1Q.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+main(){
+int i =0;
+i =400*400/400;
+cout<<i;
+}
---
+++ assignment2 3Q.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int i = 0;
+switch (1) {
+default:
+break;
+case 4:
+case 5:
+break;
+case 1:
+break;
+case 2:
+case 3:
+break;
+}
+i--;
+}
+
